222e91f38d1693b121cf0316c1ff48b80c8f862f
milap-magar <milapmagar12@gmail.com>
2024-02-03 10:03:08 +0545

    backend check

--- App.js
+++ App.js
@@ -15,6 +15,23 @@ app.use(cors({
 app.use(express.json());
 app.use('/auth', router.adminRouter)
 
+
+// Middleware to check authentication
+const authenticateUser = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next(); // User is authenticated, proceed to the next middleware or route handler
+  } else {
+    res.redirect('/auth/login'); // Redirect unauthenticated users to the login page
+  }
+};
+
+// Dashboard route with authentication middleware
+app.get('/auth/dashboard', authenticateUser, (req, res) => {
+  // Render the dashboard for authenticated users
+  res.render('dashboard');
+});
+
+
 const Port = "8080";
 
 app.listen(Port, () => {
