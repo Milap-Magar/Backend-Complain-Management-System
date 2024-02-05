5eedeeb14d70bee1b1c90e6a86883cb4e26fa1d5
milap-magar <milapmagar12@gmail.com>
2024-02-05 17:24:49 +0545

    query updated

--- App.js
+++ App.js
@@ -1,61 +1,32 @@
-// const db = require('./database/database');
 const express = require("express");
 const cors = require("cors");
-const router = require("./Routes/AdminRoutes");
-// const mysql = require('mysql');
+// const router = require("./Routes/AdminRoutes");
 
 const app = express();
 
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  methods: ['GET','POST', 'PUT'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use('/auth', router.adminRouter)
+const adminRouter = require("./Routes/AdminRoutes");
+app.use("/auth", adminRouter);
 
-
-// Middleware to check authentication
 const authenticateUser = (req, res, next) => {
   if (req.isAuthenticated()) {
-    return next(); // User is authenticated, proceed to the next middleware or route handler
+    return next();
   } else {
-    res.redirect('/auth/login'); // Redirect unauthenticated users to the login page
+    res.redirect("/auth/login");
   }
 };
-
-// Dashboard route with authentication middleware
-app.get('/auth/dashboard', authenticateUser, (req, res) => {
-  // Render the dashboard for authenticated users
-  res.render('dashboard');
+app.get("/auth/dashboard", authenticateUser, (req, res) => {
+  res.render("dashboard");
 });
-
 
 const Port = "8080";
 
-app.listen(Port, () => {
-  console.log(`🚀 ~ Listening to ${Port} ~ CONNECTED`);
-});
-
-
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user : "root",
-//     password:"",
-//     database: "login"
-// }) 
-
-// app.get("/", (req, res) => {
-//   return res.json("Backend Code");
-// });
-
-// app.get("/login", (req, res) => {
-//   const sql = "SELECT * FROM students";
-//   db.query(sql, (err, data) => {
-//     if (err) {
-//       return res.json(err);
-//     }
-//     return res.json(data);
-//   });
-// });
+app.listen(Port, () => {});
--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -4,14 +4,19 @@ const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
+// router.get("/adminlogin",(req,res)=>{
+//   res.send("here am i");
+// })
+
 router.post("/adminlogin", (req, res) => {
-  const sql = "SELECT * FROM students WHERE symbol = ? and password = ?";
-  conn.query(sql, [req.body.symbol, req.body.password], (err, result) => {
+  const sql = "SELECT * FROM admins WHERE admin_no = ? and password = ?";
+  conn.query(sql, [req.body.admin_no, req.body.password], (err, result) => {
+    console.log("🚀 ~ conn.query ~ result:", result);
     if (err) return res.json({ loginStatus: false, Error: "Query error" });
     if (result.length > 0) {
-      const symbol = result[0].symbol;
+      const adminId = result[0].admin_no;
       const token = jwt.sign(
-        { role: "admin", symbol: symbol },
+        { role: "admin", admin_no: adminId },
         "jwt_secret_key",
         { expiresIn: "1d" }
       );
@@ -23,4 +28,4 @@ router.post("/adminlogin", (req, res) => {
   });
 });
 
-module.exports = { adminRouter: router };
+module.exports = router;
