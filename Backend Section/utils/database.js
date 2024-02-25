42453eed5e84c099dda30db547007179c0e40719
milap-magar <milapmagar12@gmail.com>
2024-02-25 10:24:56 +0545

    backend

---
+++ Backend Section/utils/database.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
+
+const conn = mysql.createConnection({
+  host: process.env.DATABSE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.log("🚀 ~ DATABASE ~ connection: ERROR");
+  } else {
+    console.log("🚀 ~ DATABASE ~ Connected");
+  }
+});
+
+module.exports = conn;
